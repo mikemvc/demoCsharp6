be647b9fdc6b11b305ce5601e79ad61019cc8025
mike <mikemvc@hotmail.com>
2017-02-24 20:09:50 +0800

    demo1

---
+++ demoCsharp6/demoCsharp6Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoCsharp6Console
+{
+    class Program
+    {
+        #region demo1
+        public static string loadConnectionString(string connString)
+        {
+            if (string.IsNullOrEmpty(connString))
+            {
+                throw new ArgumentException("你又忘記傳送參數了，笨蛋", nameof(connString));
+                // 以前的寫法為
+                // throw new ArgumentException("你又忘記傳送參數了，笨蛋", "connString");
+            }
+
+            var result = System.Configuration.ConfigurationManager.ConnectionStrings[connString].ConnectionString;
+            return result;
+
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            #region demo1 nameof
+            //string connString = loadConnectionString("");
+            #endregion
+        }
+    }
+}
